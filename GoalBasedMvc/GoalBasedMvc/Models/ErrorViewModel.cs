4b033c641fa12ca57b08bf582905333f07f290ea
bwynne <bill.wynne@heidmar.com>
2018-10-08 15:47:22 -0400

    Initial Commit MVC Project Template

---
+++ GoalBasedMvc/GoalBasedMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoalBasedMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
