6d3940794391decab1165a8ce1a9d65b92c82f5b
bwynne <bill.wynne@heidmar.com>
2018-11-01 09:45:12 -0400

    Added Instructions

--- GoalBasedMvc/GoalBasedMvc/Controllers/NetworkController.cs
+++ GoalBasedMvc/GoalBasedMvc/Controllers/NetworkController.cs
@@ -40,6 +40,11 @@ namespace GoalBasedMvc.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public IActionResult Instructions()
+        {
+            return View();
+        }
+
 
 }
 }
