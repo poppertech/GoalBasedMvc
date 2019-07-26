6e51ead1b2f8feef951314cc30e3b7d41a92510b
poppertech <bwynne@poppertech.com>
2019-07-26 14:14:31 -0400

    Added Validation Annotations to NetworkEditViewModel and subcomponents

--- GoalBasedMvc/GoalBasedMvc/Models/DistributionEditViewModel.cs
+++ GoalBasedMvc/GoalBasedMvc/Models/DistributionEditViewModel.cs
@@ -1,5 +1,8 @@
-﻿namespace GoalBasedMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoalBasedMvc.Models
 {
+    [DistributionValidation]
     public class DistributionEditViewModel
     {
         public int Id { get; set; }
@@ -9,5 +12,24 @@
         public double Likely { get; set; }
         public double Best { get; set; }
         public double Maximum { get; set; }
+    }
+
+    public class DistributionValidationAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if(value is Distribution)
+            {
+                var distribution = (DistributionEditViewModel)value;
+                var isValid = true;
+                isValid = isValid && distribution.Minimum < distribution.Worst;
+                isValid = isValid && distribution.Worst < distribution.Likely;
+                isValid = isValid && distribution.Likely < distribution.Best;
+                isValid = isValid && distribution.Best < distribution.Maximum;
+                if (!isValid)
+                    return false;
+            }
+            return base.IsValid(value);
+        }
     }
 }
--- GoalBasedMvc/GoalBasedMvc/Models/NetworkEditViewModel.cs
+++ GoalBasedMvc/GoalBasedMvc/Models/NetworkEditViewModel.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GoalBasedMvc.Models
 {
     public class NetworkEditViewModel
     {
+        [Required]
+        [NodesValidation]
         public SortedDictionary<int, NodeEditViewModel> Nodes { get; set; }
+
+        [Required]
+        [CashFlowsValidation]
         public IList<CashFlow> CashFlows { get; set; }
+    }
+
+    public class NodesValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if(value is SortedDictionary<int, NodeEditViewModel>)
+            {
+                var dictionary = (SortedDictionary<int, NodeEditViewModel>)value;
+                if (dictionary.Count < 1 || dictionary.Keys.Any(k => k < 1))
+                    return false;
+            }
+            return base.IsValid(value);
+        }
+    }
+
+    public class CashFlowsValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if(value is IList<CashFlow>)
+            {
+                var list = (IList<CashFlow>)value;
+                if (list.Count < 1)
+                    return false;
+            }
+            return base.IsValid(value);
+        }
     }
 }
