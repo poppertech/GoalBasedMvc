8d5069ff293e746b263f866e1f741a98fafb425f
poppertech <bwynne@poppertech.com>
2020-03-02 07:58:57 -0500

    Fixed Network Domain Model Test

--- GoalBasedMvc/GoalBasedMvcTest/Models/NetworkTest.cs
+++ GoalBasedMvc/GoalBasedMvcTest/Models/NetworkTest.cs
@@ -11,28 +11,45 @@ namespace GoalBasedMvcTest.Models
     public class NetworkTest
     {
         [TestMethod]
-        public void CalculateTreeOnSuccessRunsCalculations()
+        public void CalculateOnSuccessRunsCalculations()
         {
             //arrange
             var cashFlow = new CashFlow() { Id = 3 };
             var cashFlows = new[] { cashFlow };
 
-            var parentId = 1;
-            var parent = new Mock<INode>();
-            parent.Setup(p => p.Id).Returns(parentId);
+            var isNotPortfolioNodeId = 1;
+            var isNotPortfolioNode = new Mock<INode>();
+            isNotPortfolioNode.SetupAllProperties();
+            isNotPortfolioNode.Setup(p => p.Id).Returns(isNotPortfolioNodeId);
+            isNotPortfolioNode.Setup(p => p.IsPortfolioComponent).Returns(false);
 
-            var childId = 2;
-            var child = new Mock<INode>();
-            child.Setup(c => c.Id).Returns(childId);
-            child.Setup(c => c.Parent).Returns(parent.Object);
+            var IsPortfolioNodeId = 2;
+            var isPortfolioNode = new Mock<INode>();
+            isPortfolioNode.SetupAllProperties();
+            isPortfolioNode.Setup(c => c.Id).Returns(IsPortfolioNodeId);
+            isPortfolioNode.Setup(p => p.IsPortfolioComponent).Returns(true);
 
-            IDictionary<int, IList<double>> nodeSimulations = new Dictionary<int, IList<double>> { { parentId, new List<double>() }, { childId, new List<double>() } };
-            IDictionary<int, INode> nodeDictionary = new Dictionary<int, INode> { { parentId, parent.Object }, { childId, child.Object } };
-            IList<INode> nodes = new List<INode> { parent.Object, child.Object };
+            var nodeDictionary = new Dictionary<int, INode> {
+                { isNotPortfolioNodeId, isNotPortfolioNode.Object },
+                { IsPortfolioNodeId, isPortfolioNode.Object }
+            };
+
+            var isNotPortfolioSimulation = 4D;
+            var isPortfolioSimulation = 5D;
+            var nodeSimulations = new Dictionary<int, IList<double>> {
+                { isNotPortfolioNodeId, new List<double>{isNotPortfolioSimulation } },
+                { IsPortfolioNodeId, new List<double>{ isPortfolioSimulation} }
+            };
+
+            var nodeSimulator = new Mock<INodeSimulator>();
+            nodeSimulator.Setup(s => s.SimulateNodes(
+                It.Is<IDictionary<int, INode>>(dict => dict == nodeDictionary )))
+                .Returns(nodeSimulations);
 
             var portfolio = new Mock<IPortfolio>();
-            var nodeSimulator = new Mock<INodeSimulator>();
-            nodeSimulator.Setup(s => s.SimulateNodes(It.Is<IDictionary<int, INode>>(n => n == nodeSimulations))).Returns(nodeSimulations);
+            portfolio.Setup(p => p.Init(
+                It.Is<IList<INode>>(list => list[0].Id == IsPortfolioNodeId && list.Count == 1),
+                It.Is<IList<CashFlow>>(cf => cf[0].Id == cashFlows[0].Id)));
 
             var network = new Network(portfolio.Object, nodeSimulator.Object);
             network.CashFlows = cashFlows;
@@ -43,10 +60,19 @@ namespace GoalBasedMvcTest.Models
 
             //assert
             Assert.AreEqual(portfolio.Object, network.Portfolio);
-            Assert.AreEqual(parentId, network.Nodes.Values.First().Id);
-            Assert.AreEqual(childId, network.Nodes.Values.Last().Id);
+            portfolio.Verify(p => p.Init(
+                It.Is<IList<INode>>(list => list[0].Id == IsPortfolioNodeId && list.Count == 1),
+                It.Is<IList<CashFlow>>(cf => cf[0].Id == cashFlows[0].Id)));
+
             Assert.AreEqual(cashFlow.Id, network.CashFlows.First().Id);
-            portfolio.Verify(p => p.Init(It.IsAny<IList<INode>>(), It.IsAny<IList<CashFlow>>()));
+
+            Assert.AreEqual(IsPortfolioNodeId, network.Nodes.Values.Last().Id);
+            isPortfolioNode.VerifySet(n => n.Portfolio = portfolio.Object);
+            Assert.AreEqual(isPortfolioSimulation, isPortfolioNode.Object.Simulations[0]);
+
+            Assert.AreEqual(isNotPortfolioNodeId, network.Nodes.Values.First().Id);
+            isNotPortfolioNode.VerifySet(n => n.Portfolio = It.IsAny<IPortfolio>(), Times.Never);
+            Assert.AreEqual(isNotPortfolioSimulation, isNotPortfolioNode.Object.Simulations[0]);
         }
     }
 }
